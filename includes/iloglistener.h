fe229791e558fe9156e74d5e0ef3679c1a99fb13
Pierre Fourgeaud <pierre@airtame.com>
2016-07-22 13:51:01 +0200

    Update ILogListener interface with docs

--- includes/iloglistener.h
+++ includes/iloglistener.h
@@ -1,17 +1,37 @@
-// Copyright 2016 Airtame
+// Copyright 2016 Pierre Fourgeaud
 
-#ifndef MANAGEDEVICE_LOGGING_ILOGLISTENER_H_
-#define MANAGEDEVICE_LOGGING_ILOGLISTENER_H_
+#ifndef PF_ILOGLISTENER_H_
+#define PF_ILOGLISTENER_H_
 
 #include <string>
 
 // Forward declarations.
 enum class ELogLevel;
 
+
+/**
+ * @brief The ILogListener class
+ *
+ * Define an interface to implement when creating a new LogListener.
+ * SimpleLogger provide 3 default listeners: Buffer, File, Console.
+ *
+ * Implement this class if you want to create your own listener.
+ */
 class ILogListener {
 public:
+    /**
+     * Virtual destructor.
+     */
     virtual ~ILogListener() {}
+
+    /**
+     * Pure virtual method to implement when implementing this interface.
+     * Method called when getting notified.
+     *
+     * @params iLog The string representing the messag to log
+     * @params iLevel Log level of this message
+     */
     virtual void Notify(const std::string& iLog, ELogLevel iLevel) = 0;
 };
 
-#endif  // MANAGEDEVICE_LOGGING_ILOGLISTENER_H_
+#endif // PF_ILOGLISTENER_H_
